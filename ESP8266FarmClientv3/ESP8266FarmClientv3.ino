20ecda332dfe4b8ee021765acce38d41b057b585
Nick Grieff <nickgrieff@hotmail.com>
2020-06-28 16:38:22 -0500

    Working for Sol1 and screen

--- ESP8266FarmClientv3/ESP8266FarmClientv3.ino
+++ ESP8266FarmClientv3/ESP8266FarmClientv3.ino
@@ -51,7 +51,7 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET    0 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, false);
 String panelText[3];
 
 const int MULTIPLEXER_MOISTURE =0;
